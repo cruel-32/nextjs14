47d5b2312daab52e1befb730b32ec2920f318837
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2024-08-28 16:43:42 +0900

    lint 수정중

---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+export default {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'strict',
+  trailingComma: 'all', // 기본값
+  tabWidth: 2,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  useTabs: false,
+};
